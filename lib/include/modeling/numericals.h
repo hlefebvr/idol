ad0db8b1e444f3e0d032cdba25d467a70dbf4719
Henri Lefebvre <hri.lefebvre@gmail.com>
2022-09-02 12:14:31 +0200

    add equals function

--- lib/include/modeling/numericals.h
+++ lib/include/modeling/numericals.h
@@ -6,7 +6,13 @@
 #define OPTIMIZE_NUMERICALS_H
 
 #include <limits>
+#include <cmath>
 
 static constexpr double Inf = std::numeric_limits<double>::max();
+static double TolFeas = 1e-8;
+
+static bool equals(double t_a, double t_b, double t_tolerance) {
+    return std::abs(t_a - t_b) <= t_tolerance;
+}
 
 #endif //OPTIMIZE_NUMERICALS_H
