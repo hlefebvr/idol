c4450241aea248cbc15b74ef4684fa4033a3b584
Henri Lefebvre <hri.lefebvre@gmail.com>
2022-09-02 12:14:53 +0200

    add tests for opp_player<>

--- tests/Types.test.cpp
+++ tests/Types.test.cpp
@@ -4,6 +4,16 @@
 
 #include "test_utils.h"
 
-TEST_CASE("", "") {
+TEST_CASE("opp_player", "[modeling]") {
+
+    SECTION("opp_player<PlayerT>::value should be the opposite player") {
+        CHECK(opp_player<Decision>::value == Parameter);
+        CHECK(opp_player<Parameter>::value == Decision);
+    }
+
+    SECTION("opp_player_v<PlayerT> should be the opposite player") {
+        CHECK(opp_player_v<Decision> == Parameter);
+        CHECK(opp_player_v<Parameter> == Decision);
+    }
 
 }
