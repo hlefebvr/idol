58231d545905edf906863e4b3b871ba9b5f519bf
Henri Lefebvre <hri.lefebvre@gmail.com>
2022-11-16 15:50:28 +0100

    remove all references to removed IIS original space builder

--- dev/dev_main.cpp
+++ dev/dev_main.cpp
@@ -1,28 +1,7 @@
 #include <iostream>
-#include "modeling.h"
 #include "algorithms/logs/Log.h"
-#include "modeling/expressions/LinExpr.h"
-#include "algorithms/branch-and-bound/BranchAndBound.h"
-#include "algorithms/branch-and-bound/Nodes_Basic.h"
-#include "algorithms/decomposition/Decomposition.h"
-#include "algorithms/row-generation/RowGeneration.h"
-#include "algorithms/branch-and-bound/BranchingStrategies_MostInfeasible.h"
-#include "algorithms/branch-and-bound/NodeUpdators_ByBoundVar.h"
-#include "algorithms/branch-and-bound/NodeStrategies_Basic.h"
-#include "algorithms/row-generation/RowGenerationSP.h"
-#include "algorithms/row-generation/RowGenerationOriginalSpaceBuilders_Dual.h"
-#include "algorithms/row-generation/RowGenerationOriginalSpaceBuilders_IIS.h"
-#include "algorithms/branch-and-bound/ActiveNodesManagers_Basic.h"
-#include "algorithms/column-generation/ColumnGeneration.h"
-#include "algorithms/column-generation/ColumnGenerationBranchingSchemes_RMP.h"
-#include "algorithms/branch-and-bound/NodeUpdators_ByBoundCtr.h"
-#include "algorithms.h"
-#include "algorithms/solvers/Solvers_GLPK_Simplex.h"
-#include "algorithms/callbacks/Callbacks_RowGeneration.h"
-#include "algorithms/callbacks/AlgorithmInCallback.h"
-#include "problems/KP/KP_Instance.h"
-#include "problems/robust/RobustProblem.h"
-#include "modeling/expressions/Expr.h"
+#include "algorithms/row-generation/ModelMirror.h"
+#include "modeling.h"
 
 int main() {
 
@@ -30,8 +9,6 @@ int main() {
     Log::set_color("branch-and-bound", Color::Blue);
     Log::set_color("column-generation", Color::Yellow);
     Log::set_color("row-generation", Color::Green);
-
-    Problems::Robust<2> problem;
 
     Model model;
     auto x = model.add_var(0., 1., Continuous, 0., "x");
