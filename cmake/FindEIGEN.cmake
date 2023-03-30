07d98ee81c4255d16a5c6bd4faa55033fdc1a8b6
Henri Lefebvre <hri.lefebvre@gmail.com>
2023-03-30 10:08:19 +0200

    use exact path for finding Eigen

--- cmake/FindEIGEN.cmake
+++ cmake/FindEIGEN.cmake
@@ -1,7 +1,7 @@
 find_path(
         EIGEN_INCLUDE_DIRS
         NAMES Eigen/Core
-        HINTS /usr/include/ /usr/local/include/  ${EIGEN_DIR} $ENV{EIGEN_HOME}
+        HINTS /usr/include/Eigen /usr/local/include/Eigen  ${EIGEN_DIR} $ENV{EIGEN_HOME}
 )
 
 include(FindPackageHandleStandardArgs)
@@ -9,5 +9,5 @@ find_package_handle_standard_args(EIGEN DEFAULT_MSG EIGEN_INCLUDE_DIRS)
 
 if (EIGEN_FOUND)
     add_library(eigen INTERFACE IMPORTED)
-    target_include_directories(eigen INTERFACE ${EIGEN_INCLUDE_DIRS}/Eigen)
+    target_include_directories(eigen INTERFACE ${EIGEN_INCLUDE_DIRS})
 endif()
