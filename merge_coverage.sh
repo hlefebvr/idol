7445380de64231bb0e79e1f3f3c26cc184a6b61f
Henri Lefebvre <hri.lefebvre@gmail.com>
2023-10-22 22:10:11 +0200

    add merge coverage script

---
+++ merge_coverage.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Create an array to store the report file names
+declare -a report_files
+
+# Add report files to the array based on your naming pattern
+for file in coverage__*.info; do
+  report_files+=("--add-tracefile" "$file")
+done
+
+# Merge the reports
+lcov "${report_files[@]}" --output coverage.info
