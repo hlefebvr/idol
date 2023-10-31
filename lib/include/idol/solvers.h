da964855df6b56a176348fa58117bb8f6434206c
Henri Lefebvre <hri.lefebvre@gmail.com>
2023-10-31 22:24:55 +0100

    update solvers.h

--- lib/include/idol/solvers.h
+++ lib/include/idol/solvers.h
@@ -2,13 +2,34 @@
 // Created by henri on 12/09/22.
 //
 
-#ifndef OPTIMIZE_optimizerS_H
-#define OPTIMIZE_optimizerS_H
+#ifndef OPTIMIZE_OPTIMIZERS_H
+#define OPTIMIZE_OPTIMIZERS_H
 
+// Branch-and-Bound
 #include "idol/optimizers/branch-and-bound/BranchAndBound.h"
-#include "idol/optimizers/archive/column-generation/ArchivedColumnGeneration.h"
+#include "idol/optimizers/branch-and-bound/branching-rules/factories/UniformlyRandom.h"
+#include "idol/optimizers/branch-and-bound/branching-rules/factories/FirstInfeasibleFound.h"
+#include "idol/optimizers/branch-and-bound/branching-rules/factories/MostInfeasible.h"
+#include "idol/optimizers/branch-and-bound/branching-rules/factories/PseudoCost.h"
+#include "idol/optimizers/branch-and-bound/branching-rules/factories/StrongBranching.h"
+#include "idol/optimizers/branch-and-bound/branching-rules/factories/LeastInfeasible.h"
+#include "idol/optimizers/branch-and-bound/node-selection-rules/factories/BreadthFirst.h"
+#include "idol/optimizers/branch-and-bound/node-selection-rules/factories/DepthFirst.h"
+#include "idol/optimizers/branch-and-bound/node-selection-rules/factories/WorstBound.h"
+#include "idol/optimizers/branch-and-bound/node-selection-rules/factories/BestBound.h"
+#include "idol/optimizers/branch-and-bound/node-selection-rules/factories/BestEstimate.h"
+
+// Dantzig-Wolfe decomposition
+#include "idol/optimizers/dantzig-wolfe/DantzigWolfeDecomposition.h"
+#include "idol/optimizers/dantzig-wolfe/infeasibility-strategies/FarkasPricing.h"
+#include "idol/optimizers/dantzig-wolfe/stabilization/NoStabilization.h"
+#include "idol/optimizers/dantzig-wolfe/stabilization/Neame.h"
+#include "idol/optimizers/dantzig-wolfe/stabilization/Wentges.h"
+
+// Wrappers
 #include "idol/optimizers/wrappers/GLPK/GLPK.h"
+#include "idol/optimizers/wrappers/HiGHS/HiGHS.h"
 #include "idol/optimizers/wrappers/Mosek/Mosek.h"
 #include "idol/optimizers/wrappers/Gurobi/Gurobi.h"
 
-#endif //OPTIMIZE_optimizerS_H
+#endif //OPTIMIZE_OPTIMIZERS_H
