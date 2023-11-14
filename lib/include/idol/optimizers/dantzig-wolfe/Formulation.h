4d596f3cd8f27d5ead1acbba13d0ac21831b1b1e
Henri Lefebvre <hri.lefebvre@gmail.com>
2023-11-14 13:59:20 +0100

    allow to pass decomposition by variable in DantzigWolfeDecomposition

--- lib/include/idol/optimizers/dantzig-wolfe/Formulation.h
+++ lib/include/idol/optimizers/dantzig-wolfe/Formulation.h
@@ -43,7 +43,11 @@ class idol::DantzigWolfe::Formulation {
     void apply_sub_problem_bound_on_master(bool t_is_lb, const idol::Var &t_var, unsigned int t_sub_problem_id, double t_value);
     LinExpr<Var> reformulate_sub_problem_variable(const Var &t_var, unsigned int t_sub_problem_id);
 public:
-    Formulation(const Model& t_original_formulation, Annotation<Ctr, unsigned int> t_decomposition);
+    Formulation(const Model& t_original_formulation, const Annotation<Ctr, unsigned int>& t_decomposition);
+
+    Formulation(const Model& t_original_formulation,
+                Annotation<Ctr, unsigned int> t_ctr_decomposition,
+                Annotation<Var, unsigned int> t_var_decomposition);
 
     Model& master() { return m_master; }
     const Model& master() const { return m_master; }
