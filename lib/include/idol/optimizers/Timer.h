b339319246a6d09b1340c00f25dc7be576bacf02
Henri Lefebvre <hri.lefebvre@gmail.com>
2024-02-28 19:58:22 +0100

    add doc for Timer

--- lib/include/idol/optimizers/Timer.h
+++ lib/include/idol/optimizers/Timer.h
@@ -11,6 +11,9 @@ namespace idol {
     class Timer;
 }
 
+/**
+ * This class is used to measure the time it takes for a certain operation to complete.
+ */
 class idol::Timer {
     using time = std::chrono::high_resolution_clock::time_point;
     using duration = std::chrono::duration<double>;
@@ -22,10 +25,31 @@ class idol::Timer {
     [[nodiscard]] duration as_duration() const;
 public:
     enum Unit { Seconds, Milliseconds, Microseconds };
+
+    /**
+     * Starts the timer.
+     */
     void start();
+
+    /**
+     * Stops the timer.
+     */
     void stop();
+
+    /**
+     * Returns the time elapsed since the timer was started.
+     * @param t_unit the unit of time to return the elapsed time in.
+     * @return the time elapsed since the timer was started.
+     */
     [[nodiscard]] double count(Unit t_unit = Seconds) const;
+
+    /**
+     * Returns the cumulative elapsed time
+     * @param t_unit the unit of time to return the elapsed time in.
+     * @return the cumulative elapsed time
+     */
     [[nodiscard]] double cumulative_count(Unit t_unit = Seconds) const;
+
     [[nodiscard]] double factor(Unit t_unit) const;
 };
 
