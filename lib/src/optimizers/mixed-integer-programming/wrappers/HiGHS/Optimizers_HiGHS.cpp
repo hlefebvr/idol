36621343724deb4d4addaf99df83ff4ec6920a37
Henri Lefebvre <hri.lefebvre@gmail.com>
2024-06-19 13:32:42 +0200

    fix version of HiGHS in tests

--- lib/src/optimizers/mixed-integer-programming/wrappers/HiGHS/Optimizers_HiGHS.cpp
+++ lib/src/optimizers/mixed-integer-programming/wrappers/HiGHS/Optimizers_HiGHS.cpp
@@ -391,6 +391,7 @@ void idol::Optimizers::HiGHS::run_without_presolve() {
         return;
     }
     m_model.setOptionValue("presolve", "off");
+    //m_model.setOptionValue("solver", "simplex");
     auto result = m_model.run();
     m_model.setOptionValue("presolve", old_presolve_setting);
 
