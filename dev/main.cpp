1a5ce93ea81c42fc8505e63479b2355b1b89d740
Henri Lefebvre <hri.lefebvre@gmail.com>
2025-03-28 18:34:52 +0100

    add shortcut to create GenerationPattern

--- dev/main.cpp
+++ dev/main.cpp
@@ -31,86 +31,27 @@
 
 using namespace idol;
 
+GenerationPattern<Ctr> operator!(const Var& t_var) {
+    GenerationPattern<Ctr> cut;
+    cut.constant().linear().set(t_var, 1);
+    return cut;
+}
+
 int main(int t_argc, const char** t_argv) {
 
     Env env;
 
-    // Read instance
-    const auto instance = Problems::FLP::read_instance_1991_Cornuejols_et_al("/home/henri/Research/idol/examples/robust/robust-facility-location.data.txt");
-    const unsigned int n_customers = instance.n_customers();
-    const unsigned int n_facilities = instance.n_facilities();
-
-    // Uncertainty set
-    Model uncertainty_set(env);
-    const double Gamma = 5;
-    const auto xi = uncertainty_set.add_vars(Dim<1>(n_customers), 0, 1, Continuous, 0., "xi");
-    uncertainty_set.add_ctr(idol_Sum(i, Range(n_customers), xi[i]) <= Gamma);
-
-    // Make model
     Model model(env);
+    const auto x = model.add_vars(Dim<1>(10), 0, 1, Continuous, 0, "x");
+    const auto y = model.add_vars(Dim<1>(10), 0, 1, Continuous, 0, "y");
 
-    Robust::Description robust_description(uncertainty_set);
-    Bilevel::Description bilevel_description(env);
+    auto cut = 1 + 2 * !y[0] + !y[1] + (1 + 2 * !y[7] + !y[0]) * x[0] + x[2] * !y[2] + 3;
 
-    const auto x = model.add_vars(Dim<1>(n_facilities), 0., 1., Binary, 0., "x");
-    const auto y = model.add_vars(Dim<2>(n_facilities, n_customers), 0., Inf, Continuous, 0., "y");
-
-    for (unsigned int i = 0 ; i < n_facilities ; ++i) {
-        const auto c = model.add_ctr(idol_Sum(j, Range(n_customers), y[i][j]) <= instance.capacity(i) * x[i], "capacity_" + std::to_string(i));
-        bilevel_description.make_lower_level(c);
-    }
-
-    for (unsigned int j = 0 ; j < n_customers ; ++j) {
-        const auto c = model.add_ctr(idol_Sum(i, Range(n_facilities), y[i][j]) >= instance.demand(j), "demand_" + std::to_string(j));
-        bilevel_description.make_lower_level(c);
-        robust_description.set_uncertain_rhs(c, 0.2 * instance.demand(j) * xi[j]);
-    }
-
-    // Set second-stage variables
-    for (unsigned int i = 0 ; i < n_facilities ; ++i) {
-        for (unsigned int j = 0 ; j < n_customers ; ++j) {
-            bilevel_description.make_lower_level(y[i][j]);
-            model.set_var_ub(y[i][j], 1.2 * instance.demand(j));
-        }
-    }
-
-    model.set_obj_expr(idol_Sum(i, Range(n_facilities),
-                                instance.fixed_cost(i) * x[i]
-                                + idol_Sum(j, Range(n_customers),
-                                           instance.per_unit_transportation_cost(i, j) *
-                                           instance.demand(j) *
-                                           y[i][j]
-                                )
-                       )
-    );
-
-    model.use(Gurobi());
-    model.optimize();
-
-    std::cout << "Deterministic Problem has value: " << model.get_best_obj() << std::endl;
-
-    const auto bilevel_optimizer =
-            Bilevel::KKT()
-                .with_single_level_optimizer(
-                        Gurobi()
-                            .with_presolve(false)
-                            .with_logs(false)
-                )
-    ;
-
-    model.use(
-            Robust::ColumnAndConstraintGeneration(robust_description, bilevel_description)
-                    .with_master_optimizer(Gurobi())
-                    //.with_initial_scenario_by_minimization(Gurobi())
-                    //.with_initial_scenario_by_maximization(Gurobi())
-                    .add_feasibility_separation_optimizer(bilevel_optimizer)
-                    .add_optimality_separation_optimizer(bilevel_optimizer)
-                    .with_logs(true)
-    );
-    model.optimize();
-
-    std::cout << "Status is " << model.get_status() << std::endl;
-    std::cout << "Two-stage Robust Problem has value: " << model.get_best_obj() << std::endl;
+    std::cout << x[0] * x[0] << std::endl;
+    std::cout << cut << std::endl;
+    std::cout << !!x[0] << std::endl;
+    std::cout << !cut << std::endl;
+    std::cout << !!cut << std::endl;
 
     return 0;
 }
--- lib/include/idol/general/utils/GenerationPattern.h
+++ lib/include/idol/general/utils/GenerationPattern.h
@@ -17,10 +17,9 @@ template<class GeneratedT,
          class CoefficientT = idol::Var
          >
 class idol::GenerationPattern {
-    using KeyT = std::conditional_t<std::is_same_v<GeneratedT, Var>, Ctr, Var>;
-    idol::AffExpr<CoefficientT> m_constant;
-    SparseVector<KeyT, idol::AffExpr<CoefficientT>> m_linear;
 public:
+    using KeyT = std::conditional_t<std::is_same_v<GeneratedT, Var>, Ctr, Var>;
+
     GenerationPattern() = default;
 
     GenerationPattern(idol::AffExpr<CoefficientT> t_constant, SparseVector<KeyT, idol::AffExpr<CoefficientT>> t_linear)
@@ -54,6 +53,171 @@ public:
         return result;
     }
 
+    GenerationPattern operator+=(const GenerationPattern& t_other) {
+        m_constant += t_other.m_constant;
+        m_linear += t_other.m_linear;
+        return *this;
+    }
+
+    GenerationPattern operator+=(double t_scalar) {
+        m_constant += t_scalar;
+        return *this;
+    }
+
+    GenerationPattern operator-=(const GenerationPattern& t_other) {
+        m_constant -= t_other.m_constant;
+        m_linear -= t_other.m_linear;
+        return *this;
+    }
+
+    GenerationPattern operator*=(const double t_scalar) {
+        m_constant *= t_scalar;
+        m_linear *= t_scalar;
+        return *this;
+    }
+
+    GenerationPattern operator/=(const double t_scalar) {
+        m_constant /= t_scalar;
+        m_linear /= t_scalar;
+        return *this;
+    }
+
+    GenerationPattern operator*=(const KeyT& t_key) {
+        if (!m_linear.empty()) {
+            throw Exception("Trying to build a quadratic generation pattern.");
+        }
+        if (m_constant.is_zero(Tolerance::Sparsity)) {
+            return *this;
+        }
+        m_linear.set(t_key, m_constant);
+        m_constant.clear();
+        return *this;
+    }
+
+protected:
+    idol::AffExpr<CoefficientT> m_constant;
+    SparseVector<KeyT, idol::AffExpr<CoefficientT>> m_linear;
 };
+
+template<class GeneratedT, class CoefficientT>
+std::ostream& operator<<(std::ostream& t_os, const idol::GenerationPattern<GeneratedT, CoefficientT>& t_gen) {
+    const auto print_constant = [&](const idol::AffExpr<CoefficientT>& t_constant) {
+
+        t_os << t_constant.constant();
+
+        for (const auto& [key, value] : t_constant.linear()) {
+            t_os << " + " << value << " !" << key;
+        }
+
+    };
+
+    print_constant(t_gen.constant());
+    for (const auto& [key, value] : t_gen.linear()) {
+        t_os << " + ";
+        if (value.linear().empty()) {
+            print_constant(value);
+        } else {
+            t_os << "(";
+            print_constant(value);
+            t_os << ")";
+        }
+        t_os << " " << key;
+    }
+
+    return t_os;
+}
+
+template<class GeneratedT, class CoefficientT>
+idol::GenerationPattern<GeneratedT, CoefficientT> operator*(const idol::GenerationPattern<GeneratedT, CoefficientT>& t_gen, const typename idol::GenerationPattern<GeneratedT, CoefficientT>::KeyT& t_key) {
+    auto result = t_gen;
+    result *= t_key;
+    return result;
+}
+
+template<class GeneratedT, class CoefficientT>
+idol::GenerationPattern<GeneratedT, CoefficientT> operator*(const typename idol::GenerationPattern<GeneratedT, CoefficientT>::KeyT& t_key, const idol::GenerationPattern<GeneratedT, CoefficientT>& t_gen) {
+    auto result = t_gen;
+    result *= t_key;
+    return result;
+}
+
+template<class GeneratedT, class CoefficientT>
+idol::GenerationPattern<GeneratedT, CoefficientT> operator+(const idol::GenerationPattern<GeneratedT, CoefficientT>& t_gen1, const idol::GenerationPattern<GeneratedT, CoefficientT>& t_gen2) {
+    auto result = t_gen1;
+    result += t_gen2;
+    return result;
+}
+
+template<class GeneratedT, class CoefficientT>
+idol::GenerationPattern<GeneratedT, CoefficientT> operator-(const idol::GenerationPattern<GeneratedT, CoefficientT>& t_gen1, const idol::GenerationPattern<GeneratedT, CoefficientT>& t_gen2) {
+    auto result = t_gen1;
+    result -= t_gen2;
+    return result;
+}
+
+template<class GeneratedT, class CoefficientT>
+idol::GenerationPattern<GeneratedT, CoefficientT> operator+(const idol::GenerationPattern<GeneratedT, CoefficientT>& t_gen, double t_scalar) {
+    auto result = t_gen;
+    result += t_scalar;
+    return result;
+}
+
+template<class GeneratedT, class CoefficientT>
+idol::GenerationPattern<GeneratedT, CoefficientT> operator-(const idol::GenerationPattern<GeneratedT, CoefficientT>& t_gen, double t_scalar) {
+    auto result = t_gen;
+    result -= t_scalar;
+    return result;
+}
+
+template<class GeneratedT, class CoefficientT>
+idol::GenerationPattern<GeneratedT, CoefficientT> operator+(double t_scalar, const idol::GenerationPattern<GeneratedT, CoefficientT>& t_gen) {
+    auto result = t_gen;
+    result += t_scalar;
+    return result;
+}
+
+template<class GeneratedT, class CoefficientT>
+idol::GenerationPattern<GeneratedT, CoefficientT> operator-(double t_scalar, const idol::GenerationPattern<GeneratedT, CoefficientT>& t_gen) {
+    auto result = t_gen;
+    result -= t_scalar;
+    return result;
+}
+
+template<class GeneratedT, class CoefficientT>
+idol::GenerationPattern<GeneratedT, CoefficientT> operator*(const double t_scalar, const idol::GenerationPattern<GeneratedT, CoefficientT>& t_gen) {
+    auto result = t_gen;
+    result *= t_scalar;
+    return result;
+}
+
+template<class GeneratedT, class CoefficientT>
+idol::GenerationPattern<GeneratedT, CoefficientT> operator*(const idol::GenerationPattern<GeneratedT, CoefficientT>& t_gen, const double t_scalar) {
+    auto result = t_gen;
+    result *= t_scalar;
+    return result;
+}
+
+template<class GeneratedT, class CoefficientT>
+idol::GenerationPattern<GeneratedT, CoefficientT> operator/(const idol::GenerationPattern<GeneratedT, CoefficientT>& t_gen, const double t_scalar) {
+    auto result = t_gen;
+    result /= t_scalar;
+    return result;
+}
+
+template<class GeneratedT, class CoefficientT>
+idol::GenerationPattern<GeneratedT, CoefficientT> operator!(const idol::GenerationPattern<GeneratedT, CoefficientT>& t_gen) {
+    idol::GenerationPattern<GeneratedT, CoefficientT> result;
+    result.constant().constant() = t_gen.constant().constant();
+    for (const auto& [key, value] : t_gen.constant().linear()) {
+        result.linear().set(key, result.linear().get(key) + value);
+    }
+    for (const auto& [key, coefficient] : t_gen.linear()) {
+        result.constant().linear().set(key, result.constant().linear().get(key) + coefficient.constant());
+        for (const auto& [key2, value] : coefficient.linear()) {
+            result.linear().set(key2, result.linear().get(key2) + value * key);
+        }
+    }
+    return result;
+}
 
 #endif //IDOL_GENERATIONPATTERN_H
