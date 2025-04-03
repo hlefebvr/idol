4f9ea5e8309df49186a4e6b0985080e22aa34092
Henri Lefebvre <hri.lefebvre@gmail.com>
2025-04-03 10:09:50 +0200

    use -o option for catch2

--- scripts/build_test_reports.sh
+++ scripts/build_test_reports.sh
@@ -1,9 +1,10 @@
 TEST_EXECUTABLES=$(find ../cmake-build-debug -type f -name "test_*")
+CURRENT_DIR=$(pwd)
 
 for EXEC in $TEST_EXECUTABLES
 do
   DIRECTORY=$(dirname "$EXEC")
   FILENAME=$(basename "$EXEC")
   REPORT_NAME="${FILENAME}.xml"
-  (cd $DIRECTORY && ./$FILENAME --reporter xml) > ../docs/_static/reports/$REPORT_NAME
+  (cd $DIRECTORY && ./$FILENAME --reporter xml -o $CURRENT_DIR/../docs/_static/reports/$REPORT_NAME)
 done
