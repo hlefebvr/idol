dda9906cc7f0296658bdc08c2d31398d01b1e19b
Henri Lefebvre <hri.lefebvre@gmail.com>
2025-04-04 10:58:02 +0200

    turn off parall build for debug

--- tests/bilevel/wrappers/solve_bilevel.test.cpp
+++ tests/bilevel/wrappers/solve_bilevel.test.cpp
@@ -29,9 +29,125 @@ TEST_CASE("Can solve a feasible bilevel problem", "[solving-bilevel]") {
 
 }
 
-TEST_CASE("Can update a problem and re-optimize") {
+TEST_CASE("Can update a problem and re-optimize", "[solving-bilevel]") {
 
-    SECTION("") {
+    SECTION("Can update the objective function of the upper level and re-optimize") {
+        CHECK(false);
+    }
+
+    SECTION("Can update the objective function of the lower level and re-optimize") {
+        CHECK(false);
+    }
+
+    SECTION("Can update the right-hand side of the upper level and re-optimize") {
+        CHECK(false);
+    }
+
+    SECTION("Can update the right-hand side of the lower level and re-optimize") {
+        CHECK(false);
+    }
+
+    SECTION("Can update the left-hand side of the upper level and re-optimize") {
+        CHECK(false);
+    }
+
+    SECTION("Can update the left-hand side of the lower level and re-optimize") {
+        CHECK(false);
+    }
+
+    SECTION("Can add a cut to the upper level and re-optimize") {
+        CHECK(false);
+    }
+
+    SECTION("Can add a cut to the lower level and re-optimize") {
+        CHECK(false);
+    }
+
+    SECTION("Can add a column to the upper level and re-optimize") {
+        CHECK(false);
+    }
+
+    SECTION("Can add a column to the lower level and re-optimize") {
+        CHECK(false);
+    }
+
+    SECTION("Can change the bounds of the upper level and re-optimize") {
+        CHECK(false);
+    }
+
+    SECTION("Can change the bounds of the lower level and re-optimize") {
+        CHECK(false);
+    }
+
+    SECTION("Can remove a constraint from the upper level and re-optimize") {
+        CHECK(false);
+    }
+
+    SECTION("Can remove a constraint from the lower level and re-optimize") {
+        CHECK(false);
+    }
+
+    SECTION("Can remove a variable from the upper level and re-optimize") {
+        CHECK(false);
+    }
+
+    SECTION("Can remove a variable from the lower level and re-optimize") {
+        CHECK(false);
+    }
+
+    SECTION("Can add and remove the same variable and re-optimize") {
+        CHECK(false);
+    }
+
+    SECTION("Can add and remove a different variable and re-optimize") {
+        CHECK(false);
+    }
+
+    SECTION("Can add and remove the same constraint and re-optimize") {
+        CHECK(false);
+    }
+
+    SECTION("Can add and remove a different constraint and re-optimize") {
+        CHECK(false);
+    }
+
+    SECTION("Can add a constraint which depends on a variable that is then removed") {
+        CHECK(false);
+    }
+
+    SECTION("Can add a variable which depends on a constraint that is then removed") {
+        CHECK(false);
+    }
+
+    SECTION("Can change the objective function then remove a variable which appears in the objective function") {
+        CHECK(false);
+    }
+
+    SECTION("Can update the objective function constant") {
+        CHECK(false);
+    }
+
+}
+
+TEST_CASE("Can solve a bilevel problem with infeasible upper level", "[solving-bilevel]") {
+
+    SECTION("Can retrieve the solution status") {
+        CHECK(false);
+    }
+
+}
+
+TEST_CASE("Can solve a bilevel problem with infeasible lower level", "[solving-bilevel]") {
+
+    SECTION("Can retrieve the solution status") {
+        CHECK(false);
+    }
+
+}
+
+TEST_CASE("Can solve an infeasible bilevel problem with feasible upper level and lower level", "[solving-bilevel]") {
+
+    SECTION("Can retrieve the solution status") {
         CHECK(false);
     }
 
