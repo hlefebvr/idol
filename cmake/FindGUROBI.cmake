786a85e73b4f26fb9bca74ddc3c8e16220475698
Henri Lefebvre <hri.lefebvre@gmail.com>
2025-11-27 11:57:18 +0100

    add gurobi130 and gurobi140

--- cmake/FindGUROBI.cmake
+++ cmake/FindGUROBI.cmake
@@ -6,7 +6,7 @@ find_path(
 
 find_library(
         GUROBI_LIBRARY
-        NAMES gurobi gurobi81 gurobi90 gurobi95 gurobi1000 gurobi100 gurobi110 gurobi120
+        NAMES gurobi gurobi81 gurobi90 gurobi95 gurobi1000 gurobi100 gurobi110 gurobi120 gurobi130 gurobi140
         HINTS ${GUROBI_DIR}
         PATH_SUFFIXES lib)
 
