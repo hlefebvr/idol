687401a2b7a7bb1e1b8434abedcf5caaa6ad40d3
Henri Lefebvre <hri.lefebvre@gmail.com>
2026-01-12 12:44:41 +0100

    add link to github

---
+++ lib/include/idol/robust/optimizers/critical-value-column-and-constraint-generation-algorithm/Formulation.h
@@ -0,0 +1,8 @@
+//
+// Created by henri on 09.01.26.
+//
+
+#ifndef IDOL_FORMULATION_H
+#define IDOL_FORMULATION_H
+
+#endif //IDOL_FORMULATION_H
---
+++ lib/src/robust/optimizers/critical-value-column-and-constraint-generation/CriticalValueColumnAndConstraintGeneration.cpp
@@ -0,0 +1,3 @@
+//
+// Created by henri on 09.01.26.
+//
