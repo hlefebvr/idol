bf6670ed43165abfacfcedf92bcf56e35e82522c
Henri Lefebvre <hri.lefebvre@gmail.com>
2026-03-30 18:20:41 +0200

    fix reference to cplex in FindLocalMIP.cmake

--- cmake/FindLocalMIP.cmake
+++ cmake/FindLocalMIP.cmake
@@ -1,4 +1,4 @@
-# cplex
+# Local-MIP
 find_path(
         LOCAL_MIP_INCLUDE_DIRS
         NAMES model_api/Model_API.h
@@ -21,5 +21,5 @@ find_package_handle_standard_args(LocalMIP DEFAULT_MSG
 if (LocalMIP_FOUND)
     add_library(local_mip SHARED IMPORTED)
     set_target_properties(local_mip PROPERTIES IMPORTED_LOCATION ${LOCAL_MIP_LIBRARY})
-    target_include_directories(cplex INTERFACE ${LOCAL_MIP_INCLUDE_DIRS})
+    target_include_directories(local_mip INTERFACE ${LOCAL_MIP_INCLUDE_DIRS})
 endif()
